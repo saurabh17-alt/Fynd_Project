1042a3e4e5a5ac88a1dbed745e05df9c666186a5
saurabh17-alt <72335249+saurabh17-alt@users.noreply.github.com>
2024-05-07 11:01:22 +0530

    updated sign in and sign up functionality

--- app/static/main.js
+++ app/static/main.js
@@ -11,6 +11,10 @@ let nameField_log = document.getElementById("nameField_log");
 
 
 let msglg = document.getElementById("msglg");
+signinbtn.disabled = false;
+signupbtn.disabled = false;
+
+console.log("form_box.classList",form_box.classList)
 
 if (msglg.value == "Invalid Credentials."){
     nameField.style.maxHeight = "0px";
@@ -29,10 +33,14 @@ signinbtn.addEventListener('click', () => {
     confirm_pswd.style.maxHeight= "0px";
     confirm_pswd_log.style.maxHeight = "0px";
     title.innerHTML = "Sign In";
-    form_box.style.height = "290px";
+    form_box.style.height = "40%";
     signupbtn.classList.add("disable1");
     signinbtn.classList.remove("disable1");
     form_box.classList.toggle("active");
+    if (signinbtn.disabled == false){
+        signinbtn.disabled = true;
+        signupbtn.disabled = false;
+    }
 });
 
 
@@ -42,9 +50,13 @@ signupbtn.addEventListener('click', () => {
     confirm_pswd.style.maxHeight= "65px";
     confirm_pswd_log.style.maxHeight = "65px";
     title.innerHTML = "Sign Up";
-    form_box.style.height = "340px";
+    form_box.style.height = "45%";
     signupbtn.classList.remove("disable1");
     signinbtn.classList.add("disable1");
     form_box.classList.toggle("active");
+    if (signupbtn.disabled == false){
+        signupbtn.disabled = true;
+        signinbtn.disabled = false;
+    }
 });
     
