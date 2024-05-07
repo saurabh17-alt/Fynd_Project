6a070183b4a6496824edb90e49fa0098abaa9013
saurabh17-alt <72335249+saurabh17-alt@users.noreply.github.com>
2024-05-07 11:12:37 +0530

    Update login.py

--- login.py
+++ login.py
@@ -2,8 +2,14 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-host = os.getenv("HOST")
-user = os.getenv("USER")
-password = os.getenv("PASSWORD")
-database = os.getenv("DATABASE")
+# host = os.getenv("HOST")
+# user = os.getenv("USER")
+# password = os.getenv("PASSWORD")
+# database = os.getenv("DATABASE")
+
+
+host = "buv8c6tkk8gphlfenxs4-mysql.services.clever-cloud.com"
+user = "ususkunzmtzwvsi1"
+password = "037m8FkK1VtOQVDVSXg8"
+database = "buv8c6tkk8gphlfenxs4"
 
